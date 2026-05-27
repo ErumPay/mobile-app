5995a5df276df525e7c16bb798ea030f70c94768
nahyebin <sju07305@naver.com>
2026-05-27 20:26:30 +0900

    [KAN-1300] fix: MypageLayout.tsx MypageFrame 폭 수정

--- src/features/mypage/components/MypageLayout.tsx
+++ src/features/mypage/components/MypageLayout.tsx
@@ -30,7 +30,7 @@ export function MypageFrame({
 }: MypageFrameProps) {
   return (
     <View
-      className={`flex-1 w-[360px] w-full flex-col items-start self-center ${backgroundClassName}`}
+      className={`flex-1 w-full max-w-[360px] flex-col items-start self-center ${backgroundClassName}`}
     >
       {children}
     </View>
