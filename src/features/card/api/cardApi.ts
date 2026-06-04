3176175dfd46b9c41455211824d0ea79a7536ac3
nahyebin <sju07305@naver.com>
2026-06-04 15:15:10 +0900

    [KAN-1359] 카드등록 프론트+백엔드 연결

--- src/features/card/api/cardApi.ts
+++ src/features/card/api/cardApi.ts
@@ -1,6 +1,6 @@
 import type { RegisterCardPayload, RegisteredCard } from '../types/card';
 
-const CARD_API_BASE_URL = 'http://localhost:8082';
+const CARD_API_BASE_URL = 'http://192.168.0.21:8082';
 const REGISTER_CARD_URL = `${CARD_API_BASE_URL}/api/v1/cards`;
 
 export async function registerCard(
